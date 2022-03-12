bea44f9d9604a661bccc2b9c1b007b6df2daeaa0
Simon Gredal <simon.gredal@icloud.com>
2022-03-12 10:44:11 +0100

    Import files from start.spring.io

---
+++ src/test/java/dk/krakkerz/krakheadzbackend/KrakheadzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.krakkerz.krakheadzbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrakheadzBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
