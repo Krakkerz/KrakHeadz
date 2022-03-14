e7afeb9040d036921deee49b5267a936485cfe39
frederikmahipal <fred88n5@stud.kea.dk>
2022-03-14 11:00:04 +0100

    typos

--- src/main/java/dk/krakkerz/krakheadzbackend/api/HobbyController.java
+++ src/main/java/dk/krakkerz/krakheadzbackend/api/HobbyController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/address")
-public class addressController {
+@RequestMapping("api/hobbies")
+public class HobbyController {
 
 
 
