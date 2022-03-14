bb874788ca0751b0000cf97038645b5c656d6362
frederikmahipal <fred88n5@stud.kea.dk>
2022-03-14 10:51:15 +0100

    added controller classes

---
+++ src/main/java/dk/krakkerz/krakheadzbackend/api/hobbyController.java
@@ -0,0 +1,12 @@
+package dk.krakkerz.krakheadzbackend.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/hobbies")
+public class hobbyController {
+
+
+
+}
---
+++ src/main/java/dk/krakkerz/krakheadzbackend/api/personController.java
@@ -0,0 +1,12 @@
+package dk.krakkerz.krakheadzbackend.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/persons")
+public class personController {
+
+
+
+}
